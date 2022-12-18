34737591a84cec969332e97d3a21abe892132826
yoo-hwan <youh5325@naver.com>
2022-12-18 22:23:23 +0900

    로그인 회원가입 임시개발

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 import "../../App.css";
 
+const loginDB = (username, password) => {
+  return function (dispatch, getState, { history }) {
+    axios({
+      method: "post",
+      url: "http://ac54507fe2ea9428abcb050e29baf295-2046403196.ap-northeast-2.elb.amazonaws.com/swagger-ui/index.html#/user-controller/loginUsingPOST",
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+  };
+};
+
 export default function SignInPage() {
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
   return (
     <div className="text-center m-5-auto">
       <h2>로 그 인</h2>
@@ -11,21 +28,30 @@ export default function SignInPage() {
         <p>
           <label>아이디</label>
           <br />
-          <input type="text" name="first_name" required />
+          <input
+            type="text"
+            name="first_name"
+            required
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
+          />
         </p>
         <p>
           <label>패스워드</label>
           <br />
-          <input type="password" name="password" required />
+          <input
+            type="password"
+            name="password"
+            required
+            onChange={(e) => {
+              setPwd(e.target.value);
+            }}
+          />
         </p>
         <p>
-          <button id="sub_btn" type="submit">
+          <button id="sub_btn" type="submit" onClick={() => loginDB()}>
             로그인
-          </button>
-        </p>
-        <p>
-          <button id="reg_btn" type="submit">
-            회원가입
           </button>
         </p>
       </form>
--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -1,13 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 import "../../App.css";
+
+const signUpDB = (username, password, place) => {
+  return function (dispatch, getState, { history }) {
+    axios({
+      method: "post",
+      url: "http://ac54507fe2ea9428abcb050e29baf295-2046403196.ap-northeast-2.elb.amazonaws.com/swagger-ui/index.html#/user-controller/signupUsingPOST",
+      data: {
+        username: username,
+        password: password,
+        place: place,
+      },
+    })
+      .then((res) => {
+        window.alert(res.data.result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+};
 
 export default function SignUpPage() {
   return (
     <div className="text-center m-5-auto">
       <h2>회 원 가 입</h2>
-      <form action="/calender">
+      <form action="/calendar">
         <p>
           <label>아이디</label>
           <br />
@@ -30,9 +51,13 @@ export default function SignUpPage() {
         </p>
 
         <p>
-          <button id="sub_btn" type="submit">
+          <button id="sub_btn" type="submit" onClick={() => signUpDB()}>
             가 입 하 기
           </button>
+          <br />
+          <p>
+            <Link to="/login">로그인 하기</Link>
+          </p>
         </p>
       </form>
       <footer>
